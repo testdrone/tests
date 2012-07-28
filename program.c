1390dcd5de5f37f0d4dde8b74c3c08f156fc9a33
gitghost <git@asylum>
2012-07-28 13:56:43 +0530

    Reads name. White space issue

--- program.c
+++ program.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
   char s[20];
   printf("Enter your name:\t");
+  fgets(s, 20, stdin);
+  printf("Your name is %s\n", s);
+  printf("End is %c \n", s[14]/*s[strlen(s) - 1]*/);
   return 0;
 }
