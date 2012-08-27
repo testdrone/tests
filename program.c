cc7345058b1eadf296e7a0d2380128eb8a58fb59
gitghost <git@asylum>
2012-08-27 21:37:50 +0530

    For patch

--- program.c
+++ program.c
@@ -2,10 +2,11 @@
 #include <string.h>
 
 int main() {
-  char s[20];
+  char s[20], int i;
   printf("Enter your name:\t");
   fgets(s, 20, stdin);
   printf("Your name is %s\n", s);
-  printf("End is %c \n", s[14]/*s[strlen(s) - 1]*/);
+  printf("End can be %c \n", s[14]/*s[strlen(s) - 1]*/);
+  //This is to test the patch file.
   return 0;
 }
